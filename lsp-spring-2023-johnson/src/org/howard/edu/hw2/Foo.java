39d1c25dc499d5d58a9615705f31c0019adfb540
mikey2fye <michael.johnson@bison.howard.edu>
2023-02-06 10:48:38 -0500

    Added files to read files for assignment #2.

---
+++ lsp-spring-2023-johnson/src/org/howard/edu/hw2/Foo.java
@@ -0,0 +1,13 @@
+package org.howard.edu.hw2;
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
