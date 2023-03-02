83075545568294d2a8eceb843dc8bac9d42fb1fc
mikey2fye <michael.johnson@bison.howard.edu>
2023-03-02 18:54:25 -0500

    Optimizations of the various test cases.

--- lsp-spring-2023-johnson/src/org/howard/edu/hw4/Driver.java
+++ lsp-spring-2023-johnson/src/org/howard/edu/hw4/Driver.java
@@ -19,111 +19,111 @@ public class Driver {
 		//in a try method to catch the exception
 		try { 
 			//declaring and initializing our first set
-			IntegerSet set1 = new IntegerSet();
+			IntegerSet setA = new IntegerSet();
 			//adding integers to the set with a for loop
 			for (int i = 1; i <= 20; i++) {
-				set1.add(i);
+				setA.add(i);
 			}
 			
 			//Testing the toString(), smallest(), and largest() functions
-			System.out.println("Value of Set 1 is: " + set1.toString() + "\n");
-			System.out.println("Smallest value in Set 1 is: " + set1.smallest()); 
-			System.out.println("Largest value in Set 1 is: " + set1.largest() + "\n"); 
+			System.out.println("Value of Set 1 is: " + setA.toString() + "\n");
+			System.out.println("Smallest value in Set 1 is: " + setA.smallest()); 
+			System.out.println("Largest value in Set 1 is: " + setA.largest() + "\n"); 
 			
 			//Testing the remove() function
 			System.out.println("Removing 7 from Set 1.");
-			set1.remove(7);
-			System.out.println("New value of Set 1: " + set1.toString() + "\n"); 
+			setA.remove(7);
+			System.out.println("New value of Set 1: " + setA.toString() + "\n"); 
 			
 			System.out.println("Removing 10 from Set 1.");
-			set1.remove(10);
-			System.out.println("New value of Set 1: " + set1.toString() + "\n"); 
+			setA.remove(10);
+			System.out.println("New value of Set 1: " + setA.toString() + "\n"); 
 			
 			System.out.println("Remove 100 from Set 1.");
-			set1.remove(100);
-			System.out.println("New value of Set 1: " + set1.toString() + "\n"); 
+			setA.remove(100);
+			System.out.println("New value of Set 1: " + setA.toString() + "\n"); 
 			
 			//Testing isEmpty() 
 			System.out.println("Set 1 is Empty (true/false)");
-			System.out.println(set1.isEmpty() + "\n"); 
+			System.out.println(setA.isEmpty() + "\n"); 
 			
 			//testing contains()
 			System.out.println("Set 1 contains 9? (true/false)");
-			System.out.println(set1.contains(9) + "\n"); 
+			System.out.println(setA.contains(9) + "\n"); 
 			
 			//testing length()
-			System.out.println("Length of Set 1: " + set1.length() + "\n"); 
+			System.out.println("Length of Set 1: " + setA.length() + "\n"); 
 			
 			//"resetting" the set
-			set1.clear();
-			for (int i = 1; i <= 10; i++) {
-				set1.add(i);
+			setA.clear();
+			for (int i = 1; i <= 15; i++) {
+				setA.add(i);
 			}
 			//declaring and initializing our second set
-			IntegerSet set2 = new IntegerSet();
+			IntegerSet setB = new IntegerSet();
 			//adding integers to the set with a for loop
-			for (int i = 0; i <= 20; i += 2) {
-				set2.add(i);
+			for (int i = 0; i <= 30; i += 2) {
+				setB.add(i);
 			}
-			System.out.println("Value of Set 1 is: " + set1.toString());
-			System.out.println("Value of Set 2 is: " + set2.toString());
+			System.out.println("Value of Set 1 is: " + setA.toString());
+			System.out.println("Value of Set 2 is: " + setB.toString());
 			System.out.println("We will reset the values of each list after every test." + "\n");
 			
 			//testing union()
 			System.out.println("Union of Set 1 and Set 2");
-			set1.union(set2);	
-			System.out.println(set1.toString() + "\n");	
+			setA.union(setB);	
+			System.out.println(setB.toString() + "\n");	
 			
-			set1.clear();
+			setA.clear();
 			for (int i = 1; i <= 10; i++) {
-				set1.add(i);
+				setA.add(i);
 			}
-			set2.clear();
+			setB.clear();
 			for (int i = 0; i <= 20; i += 2) {
-				set2.add(i);
+				setA.add(i);
 			}
 			
 			System.out.println("Difference of Set 1 and Set 2");
-			set1.diff(set2); 
-			System.out.println(set1.toString() + "\n"); 
-			set1.clear();
+			setA.diff(setB); 
+			System.out.println(setA.toString() + "\n"); 
+			setA.clear();
 			for (int i = 1; i <= 10; i++) {
-				set1.add(i);
+				setA.add(i);
 			}
-			set2.clear();
+			setB.clear();
 			for (int i = 0; i <= 20; i += 2) {
-				set2.add(i);
+				setB.add(i);
 			}
 			
 			System.out.println("Difference of Set 2 and Set 1");
-			set2.diff(set1); 
-			System.out.println(set2.toString() + "\n"); 
-			set1.clear();
+			setB.diff(setA); 
+			System.out.println(setB.toString() + "\n"); 
+			setA.clear();
 			for (int i = 1; i <= 10; i++) {
-				set1.add(i);
+				setA.add(i);
 			}
-			set2.clear();
+			setB.clear();
 			for (int i = 0; i <= 20; i += 2) {
-				set2.add(i);
+				setB.add(i);
 			}
 			
 			System.out.println("Intersection of Set 1 and Set 2");
-			set1.intersect(set2); 
-			System.out.println(set1.toString() + "\n"); 
-			set1.clear();
+			setA.intersect(setB); 
+			System.out.println(setA.toString() + "\n"); 
+			setA.clear();
 			for (int i = 1; i <= 10; i++) {
-				set1.add(i);
+				setA.add(i);
 			}
-			set2.clear();
+			setB.clear();
 			for (int i = 0; i <= 20; i += 2) {
-				set2.add(i);
+				setB.add(i);
 			}
 			System.out.println("Are Set 1 and Set 2 equal?");
-			System.out.println(set1.equals(set2) + "\n"); 
+			System.out.println(setA.equals(setB) + "\n"); 
 			
 			System.out.println("Catching the exception");
-			set1.clear(); 
-			set1.largest(); 
+			setA.clear(); 
+			setA.largest(); 
 		}
 		catch (IntegerSetException err) {
 			System.out.println(err); 
