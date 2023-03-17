b7a565080e74538d7099efb6262cbc342105a6d1
mikey2fye <michael.johnson@bison.howard.edu>
2023-03-16 21:06:29 -0400

    Adding code to the integer range and range files.

--- lsp-spring-2023-johnson/src/org/howard/edu/mid_term/problem51/IntegerRange.java
+++ lsp-spring-2023-johnson/src/org/howard/edu/mid_term/problem51/IntegerRange.java
@@ -1,5 +1,57 @@
 package org.howard.edu.mid_term.problem51;
 
-public class IntegerRange {
 
+public class IntegerRange implements Range {
+	
+	private int lower;
+	private int upper; 
+	
+	IntegerRange(int low, int up) {
+		this.lower = low;
+		this.upper = up;
+	}
+	
+	public int get_lower() {
+		return lower;
+	}
+	
+	public int get_upper() {
+		return upper;
+	}
+	
+	public boolean contains(int value) {
+		if (get_lower() <= value && value <= get_upper()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean overlaps(Range other) throws EmptyRangeException {
+		if (other == null) {
+			throw new EmptyRangeException("The range is empty");
+		}
+		else {
+			if (get_lower() >= other.get_lower() && (get_upper() <= other.get_upper() || get_upper() >= other.get_upper())) {
+				if (get_lower() > other.get_upper()) {
+					return false;
+				}
+				return true;
+			}
+			else if (get_lower() <= other.get_lower() && (get_upper() <= other.get_upper() || get_upper() >= other.get_upper())) {
+				if (other.get_lower() > get_upper()) {
+					return false;
+				}
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	
+	public int size() {
+		return get_upper() - get_lower();
+	}
 }
--- lsp-spring-2023-johnson/src/org/howard/edu/mid_term/problem51/Range.java
+++ lsp-spring-2023-johnson/src/org/howard/edu/mid_term/problem51/Range.java
@@ -1,5 +1,20 @@
 package org.howard.edu.mid_term.problem51;
 
 public interface Range {
-
+	
+	public class EmptyRangeException extends Exception {
+		public EmptyRangeException(String error) {
+			super(error);
+		}
+	}
+		
+	public boolean contains(int value);
+	
+	public boolean overlaps(Range other) throws EmptyRangeException;
+	
+	public int size();
+	
+	public int get_upper();
+	
+	public int get_lower();
 }
